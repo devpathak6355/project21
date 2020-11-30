7d926efa3dc71cf54c65cb27e5ae5fd17e0be511
devpathak6355 <72555966+devpathak6355@users.noreply.github.com>
2020-11-30 11:33:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var bullet;
+var wall;
+var speed;
+var weight;
+var thickness;
+var bulletRightEdge,wallLeftEdge;
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+  bullet=createSprite(50,200,50,50);
+  bullet.velocityX=speed;
+  wall=createSprite(1500,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80);
+}
+
+function draw(){
+  background(255,255,255);  
+  if(wall.x-bullet.x < (bullet.width+wall.width)/2){
+    bullet.velocityX=0;
+    var deformation=0.5* weight* speed* speed/22509;
+    if(deformation>180){
+      bullet.shapeColor=color(255,0,0);
+    }
+    if(deformation<180 && deformation>100){
+      var deformation=0.5* weight* speed* speed/22509;
+    if(deformation>180){
+      bullet.shapeColor=color(255,0,0);
+    }
+    if(deformation<180 && deformation>100){
+      bullet.shapeColor=color(230,230,0);
+    }
+    }
+    if(deformation<100){
+      bullet.shapeColor=color(0,225,0);
+    }
+  }
+  if(collided(bullet,wall)){
+    bullet.velocityX=0;
+    var damage=0.5* weight* speed* speed/(thickness* thickness* thickness);
+
+    if(damage>10){
+      wall.shapeColor(250,0,0);
+    }
+    if(damage<10){
+      wall.shapeColor(0,250,0);
+    }
+  }
+  collided();
+  drawSprites();
+}
+function collided(){
+  bulletRightEdge=bullet.x+bullet.width;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+  return false;
+}
